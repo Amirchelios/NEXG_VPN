0b2b36d386dd1e16821879c2cedd4543fd5f40bc
Amirchelios <131407771+Amirchelios@users.noreply.github.com>
2025-12-29 00:37:57 +0330

    پروکسی منیجر هوشمند

--- lib/providers/telegram_proxy_provider.dart
+++ lib/providers/telegram_proxy_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'dart:io';
 import '../models/telegram_proxy.dart';
 import '../services/telegram_proxy_service.dart';
 
@@ -19,13 +20,57 @@ class TelegramProxyProvider extends ChangeNotifier {
     notifyListeners();
 
     try {
-      _proxies = await _proxyService.fetchProxies();
+      final proxies = await _proxyService.fetchProxies();
+      _proxies = await _filterReachableProxies(proxies, maxCount: 10);
       _isLoading = false;
       notifyListeners();
     } catch (e) {
       _errorMessage = e.toString();
       _isLoading = false;
       notifyListeners();
+    }
+  }
+
+  Future<List<TelegramProxy>> _filterReachableProxies(
+    List<TelegramProxy> proxies, {
+    required int maxCount,
+  }) async {
+    const batchSize = 12;
+    final reachable = <TelegramProxy>[];
+    proxies.shuffle();
+
+    for (var i = 0; i < proxies.length; i += batchSize) {
+      if (reachable.length >= maxCount) {
+        break;
+      }
+      final batch = proxies.skip(i).take(batchSize).toList();
+      final results = await Future.wait(
+        batch.map((proxy) => _isProxyReachable(proxy)),
+      );
+      for (var j = 0; j < batch.length; j++) {
+        if (results[j]) {
+          reachable.add(batch[j]);
+          if (reachable.length >= maxCount) {
+            break;
+          }
+        }
+      }
+    }
+
+    return reachable;
+  }
+
+  Future<bool> _isProxyReachable(TelegramProxy proxy) async {
+    try {
+      final socket = await Socket.connect(
+        proxy.host,
+        proxy.port,
+        timeout: const Duration(seconds: 4),
+      );
+      socket.destroy();
+      return true;
+    } catch (_) {
+      return false;
     }
   }
 
